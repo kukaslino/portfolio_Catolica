37a7f0dd4f8aef56dceec1792cdf2caf671789bd
kukaslino <cauegirardi@gmail.com>
2024-11-17 19:19:13 -0300

    s

--- libs/SD/SD.h
+++ libs/SD/SD.h
@@ -19,8 +19,8 @@
 #include <Arduino.h>
 #endif
 
-#include "utility/SdFat.h"
-#include "utility/SdFatUtil.h"
+#include "SdFat.h"
+#include "SdFatUtil.h"
 
 #define FILE_READ O_READ
 #define FILE_WRITE (O_READ | O_WRITE | O_CREAT | O_APPEND)
